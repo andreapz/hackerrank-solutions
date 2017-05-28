14b93ba9c6d8ae9e6a5eef4c2262cd7688f1a144
Andrea Putzu <andreapz@MacBook-Pro-di-Andrea.local>
2017-05-28 14:41:11 +0200

    fix solution hash

--- SolutionHash.java
+++ SolutionHash.java
@@ -5,54 +5,54 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class SolutionHash {
-	
-	
-	
+
 	public static String[] lista(String[][] itemAssociation, int n) {
 		List<Set<String>> items = new ArrayList<>();
-		
-		for (String[] s : itemAssociation) {
-			System.out.println(s);
-		}
-		
+
 		for (String[] itemAssociationPars : itemAssociation) {
+			
+//			System.out.println(itemAssociationPars[0] + " " + itemAssociationPars[1]);
+
+			boolean b = false;
 			for (Set<String> itemSet : items) {
-				if(itemSet.contains(itemAssociationPars[0])) {
+				if (itemSet.contains(itemAssociationPars[0])) {
 					itemSet.add(itemAssociationPars[1]);
-				} else if(itemSet.contains(itemAssociationPars[1])) {
+//					System.out.println("+ " + itemAssociationPars[1]);
+					b = true;
+				} else if (itemSet.contains(itemAssociationPars[1])) {
 					itemSet.add(itemAssociationPars[0]);
-				}
-				else {
-					Set<String> s = new HashSet<>();
-					s.add(itemAssociationPars[0]);
-					s.add(itemAssociationPars[1]);
-					items.add(s);
+//					System.out.println("+ " + itemAssociationPars[0]);
+					b = true;
 				}
 			}
+			if (!b) {
+				Set<String> s = new HashSet<>();
+				s.add(itemAssociationPars[0]);
+				s.add(itemAssociationPars[1]);
+				items.add(s);
+//				System.out.println("+ " + itemAssociationPars[0] + "," + itemAssociationPars[1]);
+			}
 		}
-		
+
 		int count = 0;
 		Set<String> ret = null;
-		
-		
+
 		for (Set<String> currentSet : items) {
-			if(currentSet.size() > count) {
+			if (currentSet.size() > count) {
 				count = currentSet.size();
 				ret = currentSet;
 			}
 		}
-		
+
 		String[] list = new String[count];
-		int i = 0;
-		if(count > 0) {	
+		
+		if (count > 0) {
+			int i = 0;	
 			for (String r : ret) {
 				list[i++] = r;
-				System.out.println(list[i-1]);
 			}
 		}
-		
-		
-				
+
 		return list;
 
 	}
@@ -68,9 +68,15 @@ public class SolutionHash {
 			game[i][1] = scan.next();
 		}
 
-		lista(game, n);
-		
-
+		String[] l = lista(game, n);
+		if (l != null) {
+			for (int i = 0; i < l.length; i++) {
+				System.out.print(l[i] + ", ");
+			}
+		} else {
+			System.out.print("-");
+		}
+		System.out.println("");
 		scan.close();
 	}
 }
